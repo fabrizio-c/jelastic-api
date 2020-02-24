57593ed75765d09b554b1d82180acc57433f7f61
Fabrizio Carta <fabrizio.carta@ipsnet.it>
2020-02-24 16:46:21 +0100

    fix error on version

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 const api=require('./lib/jelastic.api.js');
 let jelastic=exports;
-jelastic.vesion=require('../package.json').version;
+jelastic.vesion=require('./package.json').version;
 jelastic.connection=api;
